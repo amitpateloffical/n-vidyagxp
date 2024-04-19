3ad73ba4169356432597e39e03ff98d645761046
Himanshu Patil <86556273+HIMANSHU22-16@users.noreply.github.com>
2024-04-19 15:10:56 +0530

    add process

--- database/seeders/ProcessSeeder.php
+++ database/seeders/ProcessSeeder.php
@@ -60,6 +60,109 @@ class ProcessSeeder extends Seeder
         $process->process_name = "New Document";
         $process->save();
 
+        $process  = new Process();
+        $process->division_id = 9;
+        $process->process_name = "New Document";
+        $process->save();
+
+        $process  = new Process();
+        $process->division_id =    10;
+        $process->process_name = "New Document";
+        $process->save();
+
+        $process  = new Process();
+        $process->division_id = 11;
+        $process->process_name = "New Document";
+        $process->save();
+
+        $process  = new Process();
+        $process->division_id = 12;
+        $process->process_name = "New Document";
+        $process->save();
+
+        $process  = new Process();
+        $process->division_id = 13;
+        $process->process_name = "New Document";
+        $process->save();
+
+        $process  = new Process();
+        $process->division_id = 14;
+        $process->process_name = "New Document";
+        $process->save();
+
+        $process  = new Process();
+        $process->division_id = 15;
+        $process->process_name = "New Document";
+        $process->save();
+
+        $process  = new Process();
+        $process->division_id = 16;
+        $process->process_name = "New Document";
+        $process->save();
+
+        $process  = new Process();
+        $process->division_id = 17;
+        $process->process_name = "New Document";
+        $process->save();
+
+        $process  = new Process();
+        $process->division_id = 18;
+        $process->process_name = "New Document";
+        $process->save();
+
+        $process  = new Process();
+        $process->division_id = 19;
+        $process->process_name = "New Document";
+        $process->save();
+
+        $process  = new Process();
+        $process->division_id = 20;
+        $process->process_name = "New Document";
+        $process->save();
+
+        $process  = new Process();
+        $process->division_id = 20;
+        $process->process_name = "New Document";
+        $process->save();
+
+        $process  = new Process();
+        $process->division_id = 21;
+        $process->process_name = "New Document";
+        $process->save();
+
+        $process  = new Process();
+        $process->division_id = 22;
+        $process->process_name = "New Document";
+        $process->save();
+
+        $process  = new Process();
+        $process->division_id = 23;
+        $process->process_name = "New Document";
+        $process->save();
+
+        $process  = new Process();
+        $process->division_id = 24;
+        $process->process_name = "New Document";
+        $process->save();
+
+        $process  = new Process();
+        $process->division_id = 25;
+        $process->process_name = "New Document";
+        $process->save();
+
+        $process  = new Process();
+        $process->division_id = 26;
+        $process->process_name = "New Document";
+        $process->save();
+
+        $process  = new Process();
+        $process->division_id = 27;
+        $process->process_name = "New Document";
+        $process->save();
+
+
+
+
         $processNames = [
             "Extension",
             "Action Item",
