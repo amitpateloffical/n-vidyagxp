1d4a08dee8abd0b8fafaf5c64f7dcaf8ee97dc76
AshishVerma777 <156294206+AshishVerma777@users.noreply.github.com>
2024-04-19 15:27:45 +0530

    done by ashish

--- app/Http/Controllers/tms/TrainerQualificationController.php
+++ app/Http/Controllers/tms/TrainerQualificationController.php
@@ -22,13 +22,12 @@ class TrainerQualificationController extends Controller
         //     return response()->redirect()->back()->withInput();
         // }
 
-
         $Trainer = new TrainerQualification();
        
         //dd($Trainer->all());
-        $Trainer->record_number=$request->record_number;
+        // $Trainer->record_number=  ((RecordNumber::first()->value('counter')) + 1);
         $Trainer->division_code=$request->division_code;
-       // $Trainer->Initiator_id=$request->record_number;
+        $Trainer->Initiator_id= Auth::user()->id;
         $Trainer->intiation_date=$request->intiation_date;
         $Trainer->assign_to=$request->assign_to;
         $Trainer->due_date=$request->record_number;
@@ -42,25 +41,45 @@ class TrainerQualificationController extends Controller
         $Trainer->initiated_through=$request->initiated_through;
         $Trainer->initiated_if_other=$request->initiated_if_other;
         $Trainer->external_agencies=$request->external_agencies;
-        $Trainer->trainer_skill_set=$request->trainer_skill_set;
-        $Trainer->serial_number=$request->serial_number;
+        $Trainer->trainer_skill_set = is_array($request->trainer_skill_set) ? implode(',', $request->trainer_skill_set) : $request->trainer_skill_set;
 
-        $Trainer->title_of_document=$request->title_of_document;
-        $Trainer->supporting_document=$request->supporting_document;
-        $Trainer->remarks=$request->remarks;
-        $Trainer->trainingQualificationStatus=$request->trainingQualificationStatus;
+        $Trainer->serial_number = is_array($request->serial_number) ? implode(',', $request->serial_number) : $request->serial_number;
+        
+        $Trainer->title_of_document = is_array($request->title_of_document) ? implode(',', $request->title_of_document) : $request->title_of_document;
+        
+        $Trainer->supporting_document = is_array($request->supporting_document) ? implode(',', $request->supporting_document) : $request->supporting_document;
+        
+        $Trainer->remarks = is_array($request->remarks) ? implode(',', $request->remarks) : $request->remarks;
+        $Trainer->trainingQualificationStatus = is_array($request->trainingQualificationStatus) ? implode(',', $request->trainingQualificationStatus) : $request->trainingQualificationStatus;
+        
+       // $Trainer->trainingQualificationStatus=$request->trainingQualificationStatus;
         $Trainer->Q_comment=$request->Q_comment;
-        $Trainer->inv_attachment=$request->inv_attachment;
+        if (!empty ($request->inv_attachment)) {
+            $files = [];
+            if ($request->hasfile('inv_attachment')) {
+                foreach ($request->file('inv_attachment') as $file) {
+                    $name = $request->name . 'inv_attachment' . rand(1, 100) . '.' . $file->getClientOriginalExtension();
+                    $file->move('upload/', $name);
+                    $files[] = $name;
+                }
+            }
+
+
+            $Trainer->inv_attachment = json_encode($files);
+        }
         $Trainer->Save();       
-
-
-
-
-
-
 
     }
 
 
+   
+
+    // public function show($id)
+    // {
+
+    // $data  =  TrainerQualification::find($id);
+    // //dd($data);
+    // return view('frontend.TMS.trainer_qualification_view',compact('data'));
+    // }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -271,6 +271,7 @@ Route::view('basic-search', 'frontend.basic-search');
 Route::get('trainer_qualification', [TrainerQualificationController::class, 'index'])->name('trainer_qualification');
 Route::post('trainer_qualification_store', [TrainerQualificationController::class, 'store'])->name('trainer_qualification_store');
 
+Route::get('trainer_qualification/show/{id}', [TrainerQualificationController::class, 'show']);
 Route::view('classroom_training', 'frontend.TMS.classroom_training');
 Route::view('classroom_training_view', 'frontend.TMS.classroom_training_view');
 Route::view('trainer_qualification_view', 'frontend.TMS.trainer_qualification_view');
