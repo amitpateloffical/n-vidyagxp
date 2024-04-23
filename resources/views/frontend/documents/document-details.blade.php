ffac5521668edb84a3ad467b68df5c54e1a92564
gautamsolankigxp <166195389+gautamsolankigxp@users.noreply.github.com>
2024-04-23 12:27:11 +0530

    chatpdf

--- resources/views/frontend/documents/document-details.blade.php
+++ resources/views/frontend/documents/document-details.blade.php
@@ -1,5 +1,11 @@
 @extends('frontend.layout.main')
 @section('container')
+    <style>
+        .bg-theme {
+            background: #f39f40d4;
+        }
+    </style>
+
     {{-- ======================================
                     DOCUMENT TRACKER
     ======================================= --}}
@@ -1003,28 +1009,47 @@
     {{-- CHAT PDF MODAL START --}}
     <button 
     style="position: sticky; bottom: 1rem; left: 95%;"
-    type="button" class="btn btn-primary rounded-circle shadow-lg" data-bs-toggle="modal" data-bs-target="#chatModal">
+    type="button" class="btn bg-theme rounded-circle shadow-lg" data-bs-toggle="modal" data-bs-target="#chatModal">
         <img src="{{ asset('user/images/ai_2814666.png') }}" alt="chatWithDoc" class="img-fluid" style="width: 2rem; height: 2.4rem;">
     </button>
     
-    <div class="modal" tabindex="-1" id="chatModal">
+    <div class="modal border-0" tabindex="-1" id="chatModal">
         <div class="modal-dialog">
           <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title">Chat With Document</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            <div class="modal-header" style="padding: 0 0 0 11px;">
+                <div class="row w-100">
+                    <div class="col-2 bg-theme d-flex justify-content-center align-items-center text-white" style="border-radius: 0.5rem 0 0;">
+                        <i class="fa-solid fa-robot fs-4"></i>
+                    </div>
+                    <div class="col-9">
+                        <h5 class="modal-title fs-6" style="padding: 0.5rem;">
+                            Supercharge PDF Conversations: Seamlessly Engage with Your PDFs!</h5>
+                    </div>
+                </div>
+              
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" style="margin-right: 0.8rem;"></button>
             </div>
             <div class="modal-body">
-              <div class="chat-content" id="chatContent">
-                
+              <div class="chat-content" id="chatContent" style="height: 15rem; overflow-y: scroll;">
               </div>
             </div>
             <div class="modal-footer">
-                <div>
-                    <input type="text" name="chatSendMessage" id="" placeholder="Enter your message here" class="form-control">
+                <div class="w-100">
+                    <textarea type="text" name="chatSendMessage" id="" placeholder="Enter your message here" class="form-control shadow-md border-0"></textarea>
                 </div>
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-primary" id="sendChatBtn">Send</button>
+              <div class="row w-100">
+                <div class="col-6">
+                    <button type="button" class="btn btn-light w-100" data-bs-dismiss="modal">Close</button>
+                </div>
+                <div class="col-6">
+                    <button type="button" class="btn bg-theme text-white w-100" id="sendChatBtn">
+                        <div class="spinner-border spinner-border-sm text-light" role="status" style="display: none">
+                            <span class="visually-hidden">Loading...</span>
+                        </div>
+                        <i class="fa-solid fa-paper-plane"></i>
+                    </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -1041,11 +1066,30 @@
             },
         };
 
-        const data = {
-            url: "https://environmentallab.doculife.co.in/public/sop.pdf"
+        function scrollToBottom() {
+            var chatContent = $('#chatContent');
+            chatContent.animate({
+                scrollTop: chatContent.prop('scrollHeight')
+            }, 500);
         }
 
-        let srcId = "src_pqRrbFb6yiIkiISAcPU7O";
+        const data = {
+            url: "{{ asset('pdfs/SOP'. $document->id .'.pdf') }}"
+        }
+
+        let srcId = "";
+
+        function getUserMessageHtml(message)
+        {
+            let html = `<div class="chat-content-user-chat bg-theme text-white p-3 rounded-3 w-50 mb-2" style="margin-left: auto;">${message}</div>`;
+            return html;
+        }
+
+        function getResponseMessageHtml(message)
+        {
+            let html = `<div class="chat-content-ai-chat bg-light p-3 rounded-3 w-50 mb-2" style="margin-right: auto;">${message}</div>`;
+            return html;
+        }
 
         async function initializeChatModal()
         {
@@ -1057,7 +1101,7 @@
 
                 console.log('res', res);
 
-                let sourceId = res.data.sourceId;
+                let srcId = res.data.sourceId;
 
             } catch (err) {
                 console.log('Error in initializeChatModal fn', err.message)
@@ -1066,40 +1110,58 @@
 
         async function sendChat()
         {
-            $('#sendChatBtn').prop('disabled', true);
-            let message = $('input[name=chatSendMessage]').val();
 
-            const chatData = {
-                "sourceId": srcId,
-                "messages": [
-                    {
-                        "role": "user",
-                        "content": message
-                    }
-                ]
+            let message = $('textarea[name=chatSendMessage]').val();
+
+            if (message && message.trim() !== '')
+            {
+                scrollToBottom()
+
+                $('#sendChatBtn').prop('disabled', true);
+                $('#sendChatBtn > i').hide();
+                $('#sendChatBtn > div').show();
+
+                const chatData = {
+                    "sourceId": srcId,
+                    "messages": [
+                        {
+                            "role": "user",
+                            "content": message
+                        }
+                    ]
+                }
+    
+                const userMsgHtml = getUserMessageHtml(message);
+                $('#chatContent').append(userMsgHtml).show('slow');
+                $('textarea[name=chatSendMessage]').val('');
+    
+                try {
+    
+                    const chatPdfEndpoint = "https://api.chatpdf.com/v1/chats/message";
+    
+                    const res = await axios.post(chatPdfEndpoint, chatData, config)
+    
+                    console.log('res', res);
+    
+                    let resMsg = res.data.content;
+    
+                    const aiResHtml = getResponseMessageHtml(resMsg);
+                    $('#chatContent').append(aiResHtml).show('slow');
+    
+                } catch (err) {
+                    console.log('Error in sendChat fn', err.message)
+                }
+                $('#sendChatBtn').prop('disabled', false);
+                $('#sendChatBtn > i').show();
+                $('#sendChatBtn > div').hide();
+
+                scrollToBottom()
             }
 
-            try {
-
-                const chatPdfEndpoint = "https://api.chatpdf.com/v1/chats/message";
-
-                const res = await axios.post(chatPdfEndpoint, chatData, config)
-
-                console.log('res', res);
-
-                let resMsg = res.data.content;
-
-                $('#chatContent').append('<p>'+ resMsg +'</p>')
-
-            } catch (err) {
-                console.log('Error in sendChat fn', err.message)
-            }
-            $('#sendChatBtn').prop('disabled', false);
-            $('input[name=chatSendMessage]').val('');
             
         }
 
-        // initializeChatModal();
+        initializeChatModal();
 
         $('#sendChatBtn').click(function() {
             sendChat();
